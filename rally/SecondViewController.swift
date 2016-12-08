0fcea879b5fcf1cb870a91719cf89c917c8b26fa
GBernero <student@gbernero.local>
2016-12-08 10:25:16 -0600

    First Commit

---
+++ rally/SecondViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SecondViewController.swift
+//  rally
+//
+//  Created by GBernero on 12/6/16.
+//  Copyright © 2016 GBernero. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
